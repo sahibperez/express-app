5cb2eddb3d643f4796643b59e3b40d0484efef4c
sahibperez <[200~gsahibperezv@hotmail.com>
2023-04-04 17:01:25 -0600

    testing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/' , (req, res) => res.send('Hello world 04-03-2023 09:25 p.m.'));
+app.get('/' , (req, res) => res.send('Hello world 04-04-2023 05:05 p.m.'));
 
 app.listen(3000, () => {
     console.log('My REST API running on port 3000');
