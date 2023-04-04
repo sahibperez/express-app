495ed998a0ef2e328fb887f19b09ecfbc66db864
sahibperez <[200~gsahibperezv@hotmail.com>
2023-04-03 21:04:50 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/' , (req, res) => res.send('Hello world 04-03-2023 08:15 p.m.'));
+app.get('/' , (req, res) => res.send('Hello world 04-03-2023 09:05 p.m.'));
 
 app.listen(3000, () => {
     console.log('My REST API running on port 3000');
